e96b9af23b074ec2d119673ba0c47a0759bca0f3
Enrico <enrico.zammitl@gmail.com>
2018-07-12 19:01:38 +0200

    Fix to markup

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<h1>E</h1><h4>quipment</h4><h1>T</h1><h4>racking</h4><h1>A</h1><h4>pplication</h4>
+<h1>E<h4>quipment</h4></h1><h1>T<h4>racking</h4></h1><h1>A<h4>pplication</h4></h1>
 <p><strong>ETA</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
