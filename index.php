e1de2e43f81ea035e28e7e7a7c26ce67954638b3
Enrico <enrico.zammitl@gmail.com>
2018-07-12 18:59:22 +0200

    Fix to name

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<h1>ReplaceMe</h1>
-<p><strong>ReplaceMe</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
+<h1>E</h1><h4>quipment</h4><h1>T</h1><h4>racking</h4><h1>A</h1><h4>pplication</h4>
+<p><strong>ETA</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
