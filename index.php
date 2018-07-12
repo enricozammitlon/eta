9fbca18bd3973f751b3ab93fc2354defd4cd9584
Enrico <enrico.zammitl@gmail.com>
2018-07-12 19:21:13 +0200

    Fix DOM

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-<h1>E<h4>quipment</h4></h1><h1>T<h4>racking</h4></h1><h1>A<h4>pplication</h4></h1>
+
+<html>
+<head>
+	<title>ETA</title>
+</head>
+
+<body>
+
+<h1>
+  E
+  <h4>quipment</h4>
+</h1>
+<h1>
+  T
+  <h4>racking</h4>
+</h1>
+<h1>
+  A
+  <h4>pplication</h4>
+</h1>
 <p><strong>ETA</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
+	
+</body>
