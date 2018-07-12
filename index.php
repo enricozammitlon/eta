6b4c08ba48884d0aa3958712a8e19f442e000c5e
Enrico <enrico.zammitl@gmail.com>
2018-07-12 19:29:47 +0200

    Testing the db

--- index.php
+++ index.php
@@ -6,23 +6,16 @@
 </head>
 
 <body>
-
-<h1>
-  E
+  <h1>E</h1>
   <h4>quipment</h4>
-</h1>
-<h1>
-  T
+  <h1>T</h1>
   <h4>racking</h4>
-</h1>
-<h1>
-  A
+  <h1>A</h1>
   <h4>pplication</h4>
-</h1>
-<p><strong>ETA</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
+  <p><strong>ETA</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
 	
  <?php
-         $dbhost = 'hrr5mwqn9zs54rg4.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+         $dbhost = 'hrr5mwqn9zs54rg4.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306';
          $dbuser = 'n7c7rrk1rwdzvs3l';
          $dbpass = 'zdan1klwh8drfa9n';
          $conn = mysql_connect($dbhost, $dbuser, $dbpass);
@@ -31,7 +24,7 @@
             die('Could not connect: ' . mysql_error());
          }
          else{
-         	echo 'Connected successfully';
+         	echo '<h2>Connected successfully</h2>';
          }
          mysql_close($conn);
       ?>
