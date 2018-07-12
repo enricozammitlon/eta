b5ec1103ba04aebc47ea30dae166df1a76849f6f
Enrico <enrico.zammitl@gmail.com>
2018-07-12 21:22:44 +0200

    Testing the db

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<title>ETA</title>
+    <title>ETA</title>
 </head>
 
 <body>
@@ -13,40 +13,38 @@
   <h1>A</h1>
   <h4>pplication</h4>
   <p><strong>ETA</strong> is an auotmated tracking system for replacement of faulty or broken parts </p>
-	
-	<?php
-		$url = getenv('JAWSDB_MARIA_URL');
-		$dbparts = parse_url($url);
+    
+    <?php
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
 
-		$hostname = $dbparts['host'];
-		$username = $dbparts['user'];
-		$password = $dbparts['pass'];
-		$database = ltrim($dbparts['path'],'/');
-
-
-		// Create connection
-		$conn = mysqli_connect($hostname, $username, $password, $database);
-
-		// Check connection
-		if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
-		echo "Connection was successfully established!";
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
 
 
+        // Create connection
+        $conn = mysqli_connect($hostname, $username, $password, $database);
 
-		echo 'Connected successfully<br />';
-		$sql = 'CREATE TABLE USERS';
-		$retval = mysql_query( $sql, $conn );
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        echo "Connection was successfully established!";
 
-		if(! $retval ) {
-		die('Could not create table: ' . mysql_error());
-		}
 
-		echo "table USERS created successfully\n";
+        $sql = 'CREATE TABLE USERS';
+        $retval = mysql_query( $sql, $conn );
 
-		mysql_close($conn);
-	?>
+        if(! $retval ) {
+          die('Could not create table: ' . mysql_error());
+        }
+
+        echo "table USERS created successfully\n";
+
+        mysql_close($conn);
+    ?>
 
 </body>
 
