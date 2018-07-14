1612383898290ab605888bbcd24d6a6a4b02ca3b
Enrico <enrico.zammitl@gmail.com>
2018-07-14 13:54:17 +0200

    Structural Changes

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
  <html>
 
   <head>
-  	<meta charset="UTF-8">
-  	<meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no">
     <title>ETA</title>
     <link href="style.css" rel="stylesheet" type="text/css">
   </head>
 
   <body>
-  	<header>
+    <header>
       <h1>E</h1>
       <p>quipment</p>
       <h1>T</h1>
@@ -16,6 +16,6 @@
       <h1>A</h1>
       <p>pplication</p>
       <br>
-        <p><strong>ETA</strong> is an automated tracking system for replacement of faulty or broken parts.</p>
+      <p><strong>ETA</strong> is an automated tracking system for replacement of faulty or broken parts.</p>
       </br>
      </header>      
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php include_once('header.php');?>
-<div class="all-content">
-  <div class="left-admin-menu">
-  <?php include_once('admin-menu.php');?>
+  <div class="all-content">
+    <div class="left-admin-menu">
+    <?php include_once('admin-menu.php');?>
+    </div>
+    <div class="main-content">
+      <h1> Here goes the actual content </h1>
+    </div>
   </div>
-  <div class="main-content">
-    <h1> Here goes the actual content </h1>
-  </div>
-</div>
 
 <?php include_once('footer.php'); ?>
