9e9a014c005d1e8063588453065f727d0e3cfe98
Enrico <enrico.zammitl@gmail.com>
2018-07-15 22:49:27 +0200

    Structural Changes

--- editRecord.php
+++ editRecord.php
@@ -5,50 +5,44 @@
 	    </div>
 	    <div class="main-content">
 	    <?php 
-		    if($_SERVER["REQUEST_METHOD"] == "POST"){?>
-	      <?php
-			$sql = 'SELECT * FROM items WHERE SERIALID =\''.$_POST["serialid"].'\' AND PRODNUM = \''.$_POST["prodnum"].'\'';
-			$retval = mysqli_query($conn,$sql);
-			if(! $retval ) {
-			  die('Could not get data: '.mysqli_error());
-			}
-
-			else{
-        ?>
-        <table border="1" style="width:100%">
-          <thead>
-            <tr>
-              <th>Serial ID</th>
-              <th>Product ID</th>
-              <th>Name</th>
-              <th>User ID of Reporter</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-              while($row = mysqli_fetch_assoc($retval)) {
-                echo "
-                  <form action='updateRecord.php' method='post'>
-                  <tr>
-                  <td contenteditable><input name='serialid'/>{$row['SERIALID']}</td>
-                  <td contenteditable><input name='prodid'/>{$row['PRODNUM']}</td>
-                  <td contenteditable><input name='name'/>{$row['NAME']}</td>
-                  <td contenteditable><input name='userid'/>{$row['USERID']}</td>
-                  <td><input type='submit' value='Save'/></td>
-                  </tr>
-                  </form>
-                  ";
-              }
-            ?>
-          </tbody>
-        </table>
-				<?php
-			}
-    }
-		?>
-      <?php 
+		    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    			$sql = 'SELECT * FROM items WHERE SERIALID =\''.$_POST["serialid"].'\' AND PRODNUM = \''.$_POST["prodnum"].'\'';
+    			$retval = mysqli_query($conn,$sql);
+    			if(! $retval ) {
+    			  die('Could not get data: '.mysqli_error());
+    			}
+    			else {?>
+            <table border="1" style="width:100%">
+              <thead>
+                <tr>
+                  <th>Serial ID</th>
+                  <th>Product ID</th>
+                  <th>Name</th>
+                  <th>User ID of Reporter</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  while($row = mysqli_fetch_assoc($retval)) {
+                    echo "
+                      <form action='updateRecord.php' method='post'>
+                      <tr>
+                      <td contenteditable><input name='serialid'/>{$row['SERIALID']}</td>
+                      <td contenteditable><input name='prodid'/>{$row['PRODNUM']}</td>
+                      <td contenteditable><input name='name'/>{$row['NAME']}</td>
+                      <td contenteditable><input name='userid'/>{$row['USERID']}</td>
+                      <td><input type='submit' value='Save'/></td>
+                      </tr>
+                      </form>
+                      ";
+                  }
+                ?>
+              </tbody>
+            </table>
+    				<?php
+    			}
+        }
         elseif($_SERVER["REQUEST_METHOD"] == "GET"){?>
-          ?>
           <table border="1" style="width:100%">
           <thead>
             <tr>
@@ -70,7 +64,6 @@
             </tr>
           </tbody>
         </table>
-
       <?php }?>
 	    </div>
 	  </div>
