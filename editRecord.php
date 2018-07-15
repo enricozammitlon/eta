118822b3560c6fb87fae6e8706e91ba90fa3b665
Enrico <enrico.zammitl@gmail.com>
2018-07-15 18:22:34 +0200

    Structural Changes

--- editRecord.php
+++ editRecord.php
@@ -6,7 +6,44 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){?>
 	    <?php include_once('admin-menu.php');?>
 	    </div>
 	    <div class="main-content">
-	      <h1> <?php echo "$_POST['id']" ?></h1>
+	      <h1> <?php echo '$_POST["id"]' ?></h1>
+	      <?php
+			$sql = 'SELECT * FROM items WHERE (SERIALID = $_POST["serialid"]) and (PRODNUM = $_POST["prodnum"]) )';
+			$retval = mysqli_query($conn,$sql);
+
+			if(! $retval ) {
+			  die('Could not get data: ' . mysqli_error());
+			  echo '<p>Error: Could not get data </p>';
+			}
+
+			else{
+        ?>
+        <table border="1" style="width:100%">
+          <thead>
+            <tr>
+              <th>Serial ID</th>
+              <th>Product ID</th>
+              <th>Name</th>
+              <th>User ID of Reporter</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              while($row = mysqli_fetch_assoc($retval)) {
+                echo "
+                  <tr>
+                  <td>{$row['SERIALID']}</td>
+                  <td>{$row['PRODNUM']}</td>
+                  <td>{$row['NAME']}</td>
+                  <td>{$row['USERID']}</td>
+                  </tr>";
+              }
+            ?>
+          </tbody>
+        </table>
+				<?php
+			}
+		?>
 	    </div>
 	  </div>
 
