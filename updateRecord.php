4aaa8cc8b9299fa5b53baee2c71ad7bb86a41174
Enrico <enrico.zammitl@gmail.com>
2018-07-18 20:02:53 +0200

    Structural Changes

--- updateRecord.php
+++ updateRecord.php
@@ -13,9 +13,14 @@ if(isset($_POST["new-record"])){
 else{
 
   $sql1 = 'DELETE FROM items WHERE SERIALID =\''.$_POST["prevserialid"].'\' AND PRODNUM = \''.$_POST["prevprodnum"].'\';';
+  $retval = mysqli_query($conn,$sql1);
 
+  if(! $retval ) {
+    die('Could not get data: ' . mysqli_error());
+    echo '<p>Error: Could not get data </p>';
+  }
 
-  $sql = $sql1.'INSERT INTO items VALUES (\''.$_POST["serialid"].'\',
+  $sql = 'INSERT INTO items VALUES (\''.$_POST["serialid"].'\',
   \''.$_POST["name"].'\',
   \''.$_POST["prodid"].'\',
   \''.$_POST["userid"].'\')';
