1aeba6ee9d34b3d864285817240119a7e69214af
Enrico <enrico.zammitl@gmail.com>
2018-07-18 20:58:00 +0200

    Structural Changes

--- login.php
+++ login.php
@@ -111,7 +111,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             }    
                             $row = mysqli_fetch_assoc($retval);
 
-                            $_SESSION['isAdmin'] = $row['ISADMIN'];      
+                            $_SESSION['isAdmin'] = $row;      
 
                             header("location: index.php");
 
