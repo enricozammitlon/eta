7579d7a3117f91ee5a88cf9a1641ab695ac30b91
Enrico <enrico.zammitl@gmail.com>
2018-07-22 17:37:36 +0200

    Structural Changes

--- editRecord.php
+++ editRecord.php
@@ -105,25 +105,35 @@
     // Loop through the FileList and render image files as thumbnails.
     for (var i = 0, f; f = files[i]; i++) {
 
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
 
       var reader = new FileReader();
 
       // Closure to capture the file information.
       reader.onload = (function(theFile) {
         return function(e) {
-
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+          /*
           var mydiv = document.getElementById("list");
           var newcontent = document.createElement('li');
-          newcontent.innerHTML = '<li>'+e.target.result+'</li>';
+          newcontent.innerHTML = ['<li>'.e.target.result.'</li>'].join('');
 
           while (newcontent.firstChild) {
               mydiv.appendChild(newcontent.firstChild);
           }
+          */
         };
       })(f);
 
       // Read in the image file as a data URL.
-      //reader.readAsDataURL(f);
+      reader.readAsDataURL(f);
     }
   }
 
